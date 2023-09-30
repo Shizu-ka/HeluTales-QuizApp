5b4cd35e829496da727ba00562d0e10932b157c5
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-10-01 01:29:42 +0700

    Update MainActivity.kt

--- app/src/main/java/myid/shizuka/rpl/MainActivity.kt
+++ app/src/main/java/myid/shizuka/rpl/MainActivity.kt
@@ -1,8 +1,12 @@
 package myid.shizuka.rpl
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,37 +14,35 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.MaterialToolbar
 import myid.shizuka.rpl.ui.theme.RplTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            RplTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        setUpViews()
     }
-}
+    fun setUpViews() {
+        setUpDrawerLayout()
+    }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    fun setUpDrawerLayout() {
+        val appBar = findViewById<MaterialToolbar>(R.id.appBar)
+        val mainDrawer = findViewById<DrawerLayout>(R.id.mainDrawer)
+        setSupportActionBar(appBar)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, mainDrawer, R.string.app_name, R.string.app_name)
+        actionBarDrawerToggle.syncState()
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RplTheme {
-        Greeting("Android")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
