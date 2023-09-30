e6587d3db914c6e268c69ae544080cdd46515b5b
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-10-01 01:29:43 +0700

    Create SignupActivity.kt

---
+++ app/src/main/java/myid/shizuka/rpl/SignupActivity.kt
@@ -0,0 +1,66 @@
+package myid.shizuka.rpl
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+
+class SignupActivity : AppCompatActivity() {
+
+    lateinit var firebaseAuth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val btnSignUp = findViewById<Button>(R.id.btnSignUp)
+        btnSignUp.setOnClickListener{
+            signUpUser()
+        }
+
+        val btnLogin = findViewById<TextView>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun signUpUser() {
+        val etEmailAddress = findViewById<EditText>(R.id.etEmailAddress)
+        val email: String = etEmailAddress.text.toString()
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val password: String = etPassword.text.toString()
+        val etConfirmPassword = findViewById<EditText>(R.id.etConfirmPassword)
+        val confirmPassword: String = etConfirmPassword.text.toString()
+
+        if (email.isBlank() || password.isBlank() || confirmPassword.isBlank()) {
+            Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (password != confirmPassword) {
+            Toast.makeText(this, "Password and Confirm Password do not match", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this) {
+                if(it.isSuccessful) {
+                    Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                else {
+                    Toast.makeText(this, "Error Creating User", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+}
