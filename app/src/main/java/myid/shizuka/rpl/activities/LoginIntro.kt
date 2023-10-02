04b8233a3e9f03836473a073e19bcd92b200ddc9
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-10-02 17:55:55 +0700

    Create LoginIntro.kt

---
+++ app/src/main/java/myid/shizuka/rpl/activities/LoginIntro.kt
@@ -0,0 +1,38 @@
+package myid.shizuka.rpl.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import myid.shizuka.rpl.R
+
+class LoginIntro : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_intro)
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null ) {
+            Toast.makeText(this, "User is already logged in!", Toast.LENGTH_SHORT).show()
+            redirect("MAIN")
+        }
+        val btnGetStarted = findViewById<Button>(R.id.btnGetStarted)
+        btnGetStarted.setOnClickListener {
+            redirect("LOGIN")
+        }
+
+    }
+
+    private fun redirect(name:String) {
+        val intent = when(name) {
+            "LOGIN" -> Intent(this, LoginActivity::class.java)
+            "MAIN" -> Intent(this, MainActivity::class.java)
+            else -> throw Exception("no path exist")
+        }
+        startActivity(intent)
+        finish()
+    }
+
+}
+
