36c304cbf30f03c75f5ad7de99bdc5a4948027bc
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-10-03 07:30:43 +0700

    Update MainActivity.kt

--- app/src/main/java/myid/shizuka/rpl/activities/MainActivity.kt
+++ app/src/main/java/myid/shizuka/rpl/activities/MainActivity.kt
@@ -1,24 +1,66 @@
 package myid.shizuka.rpl.activities
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.firebase.firestore.FirebaseFirestore
 import myid.shizuka.rpl.R
+import myid.shizuka.rpl.adapters.QuizAdapter
+import myid.shizuka.rpl.models.Quiz
 
 class MainActivity : AppCompatActivity() {
     lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     lateinit var drawerLayout: DrawerLayout
+    lateinit var adapter: QuizAdapter
+    private var quizList = mutableListOf<Quiz>()
+    lateinit var firestore: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setUpViews()
     }
     fun setUpViews() {
+        setUpFirestore()
         setUpDrawerLayout()
+        setUpRecycleView()
+    }
+
+    private fun setUpFirestore() {
+        firestore = FirebaseFirestore.getInstance()
+        val collectionReference = firestore.collection("quizzes")
+        collectionReference.addSnapshotListener { value, error ->
+            if(value == null || error != null){
+                Toast.makeText(this, "Error fetching data", Toast.LENGTH_SHORT).show()
+                return@addSnapshotListener
+            }
+            Log.d("DATA", value.toObjects(Quiz::class.java).toString())
+            quizList.clear()
+            quizList.addAll(value.toObjects(Quiz::class.java))
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun populateDummyData() {
+//        quizList.add(Quiz("11-12-2023","12-10-2023"))
+//        quizList.add(Quiz("13-10-2023","13-10-2023"))
+//        quizList.add(Quiz("14-10-2023","14-10-2023"))
+//        quizList.add(Quiz("15-10-2023","15-10-2023"))
+//        quizList.add(Quiz("16-10-2023","16-10-2023"))
+    }
+
+    private fun setUpRecycleView() {
+        adapter = QuizAdapter(this,quizList)
+        val quizRecyclerView = findViewById<RecyclerView>(R.id.quizRecyclerView)
+        quizRecyclerView.layoutManager = GridLayoutManager(this, 2)
+        quizRecyclerView.adapter = adapter
     }
 
     fun setUpDrawerLayout() {
