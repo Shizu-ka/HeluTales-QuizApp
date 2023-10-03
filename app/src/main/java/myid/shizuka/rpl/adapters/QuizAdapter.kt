8b0ec26a60ad5dbf016f46ad856b49717f56e2a0
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-10-03 07:31:02 +0700

    Create QuizAdapter.kt

---
+++ app/src/main/java/myid/shizuka/rpl/adapters/QuizAdapter.kt
@@ -0,0 +1,54 @@
+package myid.shizuka.rpl.adapters
+
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import myid.shizuka.rpl.R
+import myid.shizuka.rpl.activities.QuestionActivity
+//import myid.shizuka.rpl.activities.QuestionActivity
+import myid.shizuka.rpl.models.Quiz
+import myid.shizuka.rpl.utils.ColorPicker
+import myid.shizuka.rpl.utils.IconPicker
+
+//import myid.shizuka.rpl.utils.ColorPicker
+//import myid.shizuka.rpl.utils.IconPicker
+
+class QuizAdapter(val context: Context, val quizzes: List<Quiz>) :
+    RecyclerView.Adapter<QuizAdapter.QuizViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuizViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.quiz_item, parent, false)
+        return QuizViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: QuizViewHolder, position: Int) {
+        holder.textViewTitle.text = quizzes[position].title
+        holder.cardContainer.setCardBackgroundColor(Color.parseColor(ColorPicker.getColor()))
+        holder.iconView.setImageResource(IconPicker.getIcon())
+        holder.itemView.setOnClickListener {
+            Toast.makeText(context, quizzes[position].title, Toast.LENGTH_SHORT).show()
+            val intent = Intent(context, QuestionActivity::class.java)
+            intent.putExtra("Title", quizzes[position].title)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return quizzes.size
+    }
+
+    inner class QuizViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var textViewTitle: TextView = itemView.findViewById(R.id.quizTitle)
+        var iconView: ImageView = itemView.findViewById(R.id.quizIcon)
+        var cardContainer: CardView = itemView.findViewById(R.id.cardContainer)
+    }
+}
