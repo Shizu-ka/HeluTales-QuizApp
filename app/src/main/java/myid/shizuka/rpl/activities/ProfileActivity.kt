9a653ff85a50405b35a574c13dd909a32e923781
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-10-28 21:05:46 +0700

    Update ProfileActivity.kt

--- app/src/main/java/myid/shizuka/rpl/activities/ProfileActivity.kt
+++ app/src/main/java/myid/shizuka/rpl/activities/ProfileActivity.kt
@@ -1,7 +1,11 @@
 package myid.shizuka.rpl.activities
 
 import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
@@ -12,6 +16,7 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
@@ -30,6 +35,7 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_profile)
         setUpDrawerLayout()
+
         if (currentUser != null) {
             val etUEmailAddress = findViewById<EditText>(R.id.etUEmailAddress)
             etUEmailAddress.setText(currentUser.email)
@@ -37,10 +43,34 @@ class ProfileActivity : AppCompatActivity() {
             etUEmailAddress.isFocusable = false
             etUEmailAddress.isFocusableInTouchMode = false
         }
-        firebaseAuth = FirebaseAuth.getInstance()
+
+        val btnSendVerification = findViewById<Button>(R.id.btnSendVerification)
         val btnUpdate = findViewById<Button>(R.id.btnUpdate)
+
+        if (user?.isEmailVerified == true) {
+            val etStatus = findViewById<TextView>(R.id.etStatus)
+            btnSendVerification.alpha = 0.5f
+            btnSendVerification.isEnabled = false
+            btnUpdate.isEnabled = true
+
+            etStatus.setText("Email Status : Verified")
+            etStatus.setTextColor(Color.parseColor("#51FF0D"))
+        } else {
+            val etStatus = findViewById<TextView>(R.id.etStatus)
+            btnSendVerification.isEnabled = true
+            btnUpdate.isEnabled = false
+            btnUpdate.alpha = 0.5f
+            etStatus.setText("Email Status : Unverified")
+            etStatus.setTextColor(Color.parseColor("#FF3131"))
+        }
+
+        firebaseAuth = FirebaseAuth.getInstance()
         btnUpdate.setOnClickListener {
             updateUser()
+        }
+
+        btnSendVerification.setOnClickListener{
+            sendVerification()
         }
 
 //        val btnLogin = findViewById<TextView>(R.id.btnLogin)
@@ -52,8 +82,9 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     private fun updateUser() {
-        val etUEmailAddress = findViewById<EditText>(R.id.etUEmailAddress)
-        val email: String = etUEmailAddress.text.toString()
+        val etUNewEmailAddressError = findViewById<TextView>(R.id.etUNewEmailAddressError)
+        val etUNewPasswordError = findViewById<TextView>(R.id.etUNewPasswordError)
+//        val email: String = etUNewEmailAddressError.text.toString()
         val etUNewEmailAddress = findViewById<EditText>(R.id.etUNewEmailAddress)
         val newEmail: String = etUNewEmailAddress.text.toString()
         val etUOldPassword = findViewById<EditText>(R.id.etUOldPassword)
@@ -65,18 +96,33 @@ class ProfileActivity : AppCompatActivity() {
             Toast.makeText(this, "Email or Password can't be blank", Toast.LENGTH_SHORT).show()
             return
         }
+        if (newEmail.isBlank()) {
+            etUNewEmailAddressError.setText("Please type your old email if you doesn't want to change it")
+            return
+        }
+        if (newPassword.isBlank()) {
+            etUNewPasswordError.setText("Please type your old password if you doesn't want to change it")
+            return
+        }
 
         val credential = EmailAuthProvider.getCredential(currentUser?.email!!, oldPassword)
 
         // Reauthenticate the user with the old password
+        var isMainOpened = false
         currentUser.reauthenticate(credential).addOnCompleteListener { reauthTask ->
             if (user?.isEmailVerified == true) {
                 // Update the email
                 if (reauthTask.isSuccessful) {
-                    currentUser.updateEmail(newEmail).addOnCompleteListener { updateEmailTask ->
+                    currentUser.verifyBeforeUpdateEmail(newEmail).addOnCompleteListener { updateEmailTask ->
                         if (updateEmailTask.isSuccessful) {
                             Toast.makeText(this, "Email updated successfully", Toast.LENGTH_SHORT)
                                 .show()
+                            Toast.makeText(this, "You need to verify your new email before you can use it, check your inbox", Toast.LENGTH_SHORT)
+                                .show()
+                            if (!isMainOpened) {
+                                openMain()
+                                isMainOpened = true
+                            }
                         } else {
                             val emailErrorMessage =
                                 updateEmailTask.exception?.message ?: "Failed to update email"
@@ -91,24 +137,23 @@ class ProfileActivity : AppCompatActivity() {
                         }
                         Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
                     }
-                } else {
-                    Toast.makeText(this, "Please verify your email address", Toast.LENGTH_SHORT)
-                        .show()
-                    user.sendEmailVerification().addOnCompleteListener {
-                        Toast.makeText(this, "Please verify your email address", Toast.LENGTH_SHORT)
-                            .show()
-                    }
                 }
 
                 // Old password is correct, now update the password
                 currentUser.updatePassword(newPassword)
                     .addOnCompleteListener { updatePasswordTask ->
                         if (updatePasswordTask.isSuccessful) {
-                            Toast.makeText(
-                                this,
-                                "Password updated successfully",
-                                Toast.LENGTH_SHORT
-                            ).show()
+                            if(newPassword != oldPassword) {
+                                Toast.makeText(
+                                    this,
+                                    "Password updated successfully",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            if (!isMainOpened) {
+                                openMain()
+                                isMainOpened = true
+                            }
                         } else {
                             val passwordErrorMessage =
                                 updatePasswordTask.exception?.message ?: "Failed to update password"
@@ -127,15 +172,86 @@ class ProfileActivity : AppCompatActivity() {
         }
     }
 
+    private var isVerificationAllowed = true
+    private val verificationHandler = Handler(Looper.getMainLooper())
+    private var remainingTimeInSeconds = 60 // Initial value 60 seconds
+
+    private fun sendVerification() {
+        if (isVerificationAllowed) {
+            isVerificationAllowed = false
+            user?.sendEmailVerification()?.addOnCompleteListener {
+                Toast.makeText(this, "Verification email has been sent to your email", Toast.LENGTH_SHORT).show()
+            }
+
+            // Enable verification after 1 minute
+            verificationHandler.postDelayed({
+                isVerificationAllowed = true
+                remainingTimeInSeconds = 60
+                val etTimer = findViewById<TextView>(R.id.etTimer)
+                etTimer.setText("")
+            }, 60000) // 60000 = 1 minute
+
+            // Start countdown
+            startCountdown()
+        } else {
+            val remainingTimeMessage = "Please wait for $remainingTimeInSeconds seconds before requesting verification again."
+            Toast.makeText(this, remainingTimeMessage, Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun startCountdown() {
+        verificationHandler.post(object : Runnable {
+            override fun run() {
+                remainingTimeInSeconds--
+                if (remainingTimeInSeconds > 0) {
+                    val etTimer = findViewById<TextView>(R.id.etTimer)
+                    val remainingTimeMessage = "Please wait for $remainingTimeInSeconds seconds before requesting verification again."
+                    etTimer.setText(remainingTimeMessage)
+                    verificationHandler.postDelayed(this, 1000) // Schedule the next update after 1 second
+                }
+            }
+        })
+    }
+
+    private fun openMain() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+
     fun setUpDrawerLayout() {
         val appBar = findViewById<MaterialToolbar>(R.id.appBar)
         val mainDrawer = findViewById<DrawerLayout>(R.id.mainDrawer)
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
         setSupportActionBar(appBar)
-        actionBarDrawerToggle = ActionBarDrawerToggle(this, mainDrawer,
+        actionBarDrawerToggle = ActionBarDrawerToggle(
+            this, mainDrawer,
             R.string.app_name,
             R.string.app_name
         )
         actionBarDrawerToggle.syncState()
+        val menu = navigationView.menu
+        val highlightedItem = menu.findItem(R.id.profilePage)
+        highlightedItem.isChecked = true
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.mainPage -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.followUs -> {
+                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.shizuka.my.id"))
+                    startActivity(browserIntent)
+                }
+                R.id.rateUs -> {
+                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.shizuka.my.id"))
+                    startActivity(browserIntent)
+                }
+            }
+            mainDrawer.closeDrawers()
+            true
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -143,6 +259,11 @@ class ProfileActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        actionBarDrawerToggle.syncState()
     }
 
 }
