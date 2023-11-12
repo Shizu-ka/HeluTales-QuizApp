b3e89ef047e234cd017d49ab9dd104a10ad3185b
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-11-12 22:09:01 +0700

    Update FirebaseHelper.kt

--- app/src/main/java/myid/shizuka/rpl/adapters/FirebaseHelper.kt
+++ app/src/main/java/myid/shizuka/rpl/adapters/FirebaseHelper.kt
@@ -19,6 +19,38 @@ class FirebaseHelper {
                 }
             }
         }
+        fun fetchQuizCount(context: Context, callback: (Int) -> Unit) {
+            val firestore = FirebaseFirestore.getInstance()
+            val collectionReference = firestore.collection("quizzes")
+            collectionReference.get().addOnSuccessListener { querySnapshot ->
+                val quizCount = querySnapshot.size()
+                callback(quizCount)
+            }.addOnFailureListener { exception ->
+                Toast.makeText(context, "There is something wrong, please contact us", Toast.LENGTH_SHORT).show()
+            }
+        }
+        fun fetchAverageScore(context: Context, callback: (Int) -> Unit) {
+            val firestore = FirebaseFirestore.getInstance()
+            val collectionReference = firestore.collection("completedQuiz")
+
+            collectionReference.get().addOnSuccessListener { querySnapshot ->
+                var totalScore = 0
+                var quizCount = 0
+
+                for (document in querySnapshot) {
+                    val score = document.getDouble("score")
+                    if (score != null) {
+                        totalScore += score.toInt()
+                        quizCount++
+                    }
+                }
+
+                val averageScore = if (quizCount != 0) totalScore / quizCount else 0
+                callback(averageScore)
+            }.addOnFailureListener { exception ->
+                Toast.makeText(context, "There is something wrong, please contact us", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
 
