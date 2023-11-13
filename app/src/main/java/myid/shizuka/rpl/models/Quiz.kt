27b188e5bace54c096b31b7e746d77df097d2322
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-11-13 19:41:30 +0700

    Update Quiz.kt

--- app/src/main/java/myid/shizuka/rpl/models/Quiz.kt
+++ app/src/main/java/myid/shizuka/rpl/models/Quiz.kt
@@ -2,6 +2,20 @@ package myid.shizuka.rpl.models
 
 class Quiz {
     var id: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
     var title: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
     var questions: MutableMap<String, Question> = mutableMapOf()
+        get() = field
+        set(value) {
+            field = value
+        }
 }
