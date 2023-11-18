b5e24bd33755272a485f8f6cfb5be1c7aad3b1c9
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-11-18 21:55:07 +0700

    Update RegisterAdapter.kt

--- app/src/main/java/myid/shizuka/rpl/adapters/RegisterAdapter.kt
+++ app/src/main/java/myid/shizuka/rpl/adapters/RegisterAdapter.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import myid.shizuka.rpl.models.User
 
 class RegisterAdapter(private val context: Context, private val onSuccess: () -> Unit) {
 
@@ -20,10 +23,24 @@ class RegisterAdapter(private val context: Context, private val onSuccess: () ->
             return
         }
 
+        if (!email.endsWith("@student.ub.ac.id")) {
+            Toast.makeText(context, "Only @student.ub.ac.id emails are allowed", Toast.LENGTH_SHORT).show()
+            return
+        }
+
         firebaseAuth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+
+                    val user = User().apply {
+                        this.email = email
+                        this.password = password
+                    }
+                    val databaseR = Firebase.database.reference
+                    val userId = databaseR.child("users").push().key ?: ""
+                    databaseR.child("users").child(userId).setValue(user)
+
+                    Toast.makeText(context, "Registration Successful", Toast.LENGTH_SHORT).show()
                     onSuccess.invoke() // Invoke the success callback
                 } else {
                     Toast.makeText(context, "Error Creating User", Toast.LENGTH_SHORT).show()
