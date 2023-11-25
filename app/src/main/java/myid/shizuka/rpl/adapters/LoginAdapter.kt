35dc4190315e02385b61721be230cec7fe5db121
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-11-25 16:34:35 +0700

    Update LoginAdapter.kt

--- app/src/main/java/myid/shizuka/rpl/adapters/LoginAdapter.kt
+++ app/src/main/java/myid/shizuka/rpl/adapters/LoginAdapter.kt
@@ -1,19 +1,21 @@
 import android.content.Context
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import myid.shizuka.rpl.activities.Authentication
 import myid.shizuka.rpl.models.User
 
-class LoginAdapter(private val context: Context, private val onSuccess: () -> Unit) {
+class LoginAdapter(private val context: Context, private val onSuccess: () -> Unit): Authentication() {
 
     private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var user: User? = null
 
     fun login(email: String, password: String) {
-        val user = User().apply {
+        user = User().apply {
             this.email = email
             this.password = password
         }
 
-        if (user.email.isBlank() || user.password.isBlank()) {
+        if (user!!.email.isBlank() || user!!.password.isBlank()) {
             Toast.makeText(context, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
             return
         }
@@ -21,15 +23,20 @@ class LoginAdapter(private val context: Context, private val onSuccess: () -> Un
             Toast.makeText(context, "Only @student.ub.ac.id emails are allowed", Toast.LENGTH_SHORT).show()
             return
         }
+        auth()
+    }
 
-        firebaseAuth.signInWithEmailAndPassword(user.email, user.password)
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
-                    onSuccess.invoke() // Invoke the success callback
-                } else {
-                    Toast.makeText(context, "Login Failed", Toast.LENGTH_SHORT).show()
+    override fun auth() {
+        user?.let {
+            firebaseAuth.signInWithEmailAndPassword(it.email, user!!.password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
+                        onSuccess.invoke() // Invoke the success callback
+                    } else {
+                        Toast.makeText(context, "Login Failed", Toast.LENGTH_SHORT).show()
+                    }
                 }
-            }
+        }
     }
 }
