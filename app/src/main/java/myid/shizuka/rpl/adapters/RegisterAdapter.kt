f3e793215fefee8b6f3b8e344a0b7d12b12b0cbc
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-11-25 16:34:40 +0700

    Update RegisterAdapter.kt

--- app/src/main/java/myid/shizuka/rpl/adapters/RegisterAdapter.kt
+++ app/src/main/java/myid/shizuka/rpl/adapters/RegisterAdapter.kt
@@ -1,16 +1,17 @@
 package myid.shizuka.rpl.adapters
 
 import android.content.Context
-import android.content.Intent
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import myid.shizuka.rpl.activities.Authentication
 import myid.shizuka.rpl.models.User
 
-class RegisterAdapter(private val context: Context, private val onSuccess: () -> Unit) {
+class RegisterAdapter(private val context: Context, private val onSuccess: () -> Unit): Authentication() {
 
     private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var user: User? = null
 
     fun createUserWithEmailAndPassword(email: String, password: String, confirmPassword: String) {
         if (email.isBlank() || password.isBlank() || confirmPassword.isBlank()) {
@@ -27,24 +28,28 @@ class RegisterAdapter(private val context: Context, private val onSuccess: () ->
             Toast.makeText(context, "Only @student.ub.ac.id emails are allowed", Toast.LENGTH_SHORT).show()
             return
         }
+        user = User().apply {
+            this.email = email
+            this.password = password
+        }
+        auth()
+    }
 
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
+    override fun auth() {
+        user?.let {
+            firebaseAuth.createUserWithEmailAndPassword(it.email, user!!.password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val databaseR = Firebase.database.reference
+                        val userId = databaseR.child("users").push().key ?: ""
+                        databaseR.child("users").child(userId).setValue(user)
 
-                    val user = User().apply {
-                        this.email = email
-                        this.password = password
+                        Toast.makeText(context, "Registration Successful", Toast.LENGTH_SHORT).show()
+                        onSuccess.invoke() // Invoke the success callback
+                    } else {
+                        Toast.makeText(context, "Error Creating User", Toast.LENGTH_SHORT).show()
                     }
-                    val databaseR = Firebase.database.reference
-                    val userId = databaseR.child("users").push().key ?: ""
-                    databaseR.child("users").child(userId).setValue(user)
-
-                    Toast.makeText(context, "Registration Successful", Toast.LENGTH_SHORT).show()
-                    onSuccess.invoke() // Invoke the success callback
-                } else {
-                    Toast.makeText(context, "Error Creating User", Toast.LENGTH_SHORT).show()
                 }
-            }
+        }
     }
 }
