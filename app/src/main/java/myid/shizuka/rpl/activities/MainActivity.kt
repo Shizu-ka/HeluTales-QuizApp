e8e4e62832b65ab3258869826b9e46a0c8b2e180
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-11-28 22:34:25 +0700

    Update MainActivity.kt

--- app/src/main/java/myid/shizuka/rpl/activities/MainActivity.kt
+++ app/src/main/java/myid/shizuka/rpl/activities/MainActivity.kt
@@ -3,7 +3,10 @@ package myid.shizuka.rpl.activities
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -11,55 +14,53 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import myid.shizuka.rpl.R
-import myid.shizuka.rpl.adapters.MainAdapter
+import myid.shizuka.rpl.adapters.FirebaseHelper
 import myid.shizuka.rpl.adapters.QuizAdapter
 import myid.shizuka.rpl.models.Quiz
-import myid.shizuka.rpl.utils.ColorPicker
 import myid.shizuka.rpl.utils.DrawerUtils
-import myid.shizuka.rpl.utils.IconPicker
 
 class MainActivity : AppCompatActivity() {
-
     lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     lateinit var drawerLayout: DrawerLayout
     lateinit var adapter: QuizAdapter
     private var quizList = mutableListOf<Quiz>()
-    lateinit var mainAdapter: MainAdapter
-
+    lateinit var firestore: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setUpViews()
     }
-
-    private fun setUpViews() {
-        mainAdapter = MainAdapter(this) { quizzes ->
+    fun setUpViews() {
+        fetchDataFromFirestore()
+        setUpDrawerLayout()
+        setUpRecycleView()
+    }
+    private fun fetchDataFromFirestore() {
+        FirebaseHelper.fetchQuizzes(this) { quizzes ->
             quizList.clear()
             quizList.addAll(quizzes)
             adapter.notifyDataSetChanged()
         }
-        mainAdapter.fetchDataFromFirestore()
-
-        setUpDrawerLayout()
-        setUpRecyclerView()
     }
 
-    private fun setUpRecyclerView() {
-        adapter = QuizAdapter(this, quizList)
+
+    private fun setUpRecycleView() {
+        adapter = QuizAdapter(this,quizList)
         val quizRecyclerView = findViewById<RecyclerView>(R.id.quizRecyclerView)
         quizRecyclerView.layoutManager = GridLayoutManager(this, 2)
         quizRecyclerView.adapter = adapter
     }
 
-    private fun setUpDrawerLayout() {
+    fun setUpDrawerLayout() {
         val currentPage = "mainPage"
         val appBar = findViewById<MaterialToolbar>(R.id.appBar)
         val mainDrawer = findViewById<DrawerLayout>(R.id.mainDrawer)
         val navigationView = findViewById<NavigationView>(R.id.navigationView)
         setSupportActionBar(appBar)
-        actionBarDrawerToggle = ActionBarDrawerToggle(
-            this, mainDrawer,
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, mainDrawer,
             R.string.app_name,
             R.string.app_name
         )
@@ -67,7 +68,11 @@ class MainActivity : AppCompatActivity() {
         val menu = navigationView.menu
         val highlightedItem = menu.findItem(R.id.mainPage)
         highlightedItem.isChecked = true
-        DrawerUtils.setupNavigationDrawer(this, appBar, mainDrawer, navigationView, currentPage)
+        DrawerUtils.setupNavigationDrawer(this, appBar, mainDrawer, navigationView,currentPage)
+    }
+
+    override fun onPause() {
+        super.onPause()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -76,15 +81,9 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
-
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
         actionBarDrawerToggle.syncState()
     }
 
-    override fun onResume() {
-        super.onResume()
-        ColorPicker.resetColorIndex()
-        IconPicker.resetIconIndex()
-    }
 }
