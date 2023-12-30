5837295eac10080ec5df27113139224c1460f968
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-12-30 14:50:31 +0700

    Update Question.kt

--- app/src/main/java/myid/shizuka/rpl/models/Question.kt
+++ app/src/main/java/myid/shizuka/rpl/models/Question.kt
@@ -2,50 +2,11 @@ package myid.shizuka.rpl.models
 
 class Question {
     var description: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-
     var option1: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-
     var option2: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-
     var option3: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-
     var option4: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-
     var answer: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-
     var userAnswer: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-
     var materi: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
 }
