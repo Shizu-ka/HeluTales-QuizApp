bc9567d634e3b07b3a2de1a73c4f0c9cd38a09b3
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-12-30 14:50:35 +0700

    Update User.kt

--- app/src/main/java/myid/shizuka/rpl/models/User.kt
+++ app/src/main/java/myid/shizuka/rpl/models/User.kt
@@ -2,14 +2,5 @@ package myid.shizuka.rpl.models
 
     class User {
         var email: String = ""
-            get() = field
-            set(value) {
-                field = value
-            }
-
         var password: String = ""
-            get() = field
-            set(value) {
-                field = value
-            }
     }
